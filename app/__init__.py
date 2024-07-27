f58932c99035a84169475424aeb9819f31c8436f
manrabs <muhammedrabiuumar@gmail.com>
2024-07-27 07:02:42 -0500

    added login page and form

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 # The app variable below is defined as an instance of class Flask in the __init__.py script, which makes it a member of the app package
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # The app package below is defined by the app directory and the __init__.py script, and is referenced in the from app import routes statement.
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,32 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : 'wanrabs'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author' : {'username' : 'manrabs'},
+            'body' : 'Hurrican season in Houston stops me from seeing my love'
+        },
+        {
+            'author' : {'username' : 'Susan'},
+            'body' : 'The Avengers movie was kinda mid still'
+        },
+        {
+            'author' : {'username' : 'Yusra'},
+            'body' :' I miss us so much. When will I see my husband again?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
