855daf5272b56cd743ed55076239c0fa4549e4a7
manrabs <muhammedrabiuumar@gmail.com>
2024-07-27 23:10:50 -0500

    created and integrated database and models by using sqlalchemy, migrate, and model classes. Also created a shell context function that allows for easier testing by simply running the flask shell command in venv without having to run the necessary import commands each test instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # The app variable below is defined as an instance of class Flask in the __init__.py script, which makes it a member of the app package
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # The app package below is defined by the app directory and the __init__.py script, and is referenced in the from app import routes statement.
-from app import routes
+from app import routes, models
--- miniblog.py
+++ miniblog.py
@@ -1,1 +1,8 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def createShellContext():
+    return{'sa':sa, 'so':so, 'db':db, 'User':User, 'Post':Post} 
